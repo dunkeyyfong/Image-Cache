9f718e43e02717a4e050f1a56fe219e4c3c9a19e
Fong <126961584+dunkeyyfong@users.noreply.github.com>
2023-12-25 15:30:09 +0700

    Initial Commit

---
+++ APICalls/APICallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  APICallsApp.swift
+//  APICalls
+//
+//  Created by DunkeyyFong on 25/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct APICallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
